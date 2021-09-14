ad38603c17151f93fb9faa059addb0cfcae74efb
anime republic <89608198+anime-republic@users.noreply.github.com>
2021-09-14 14:18:19 +0800

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -136,7 +136,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n\n<b>FREE:</b> <code>{free}</code>\n<b>UPTIME:</b> <code>{currentTime}</code>\n\n<b>🔺:</b> <code>{ulspeed}/s</code>\<b>🔻:</b> <code>{dlspeed}/s</code>\n"
+        msg += f"\n\n<b>FREE:</b> <code>{free}</code>\n<b>UPTIME:</b> <code>{currentTime}</code>\n\n<b>🔺:</b> <code>{ulspeed}/s</code>\n<b>🔻:</b> <code>{dlspeed}/s</code>\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -179,7 +179,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n\n<b>FREE:</b> <code>{free}</code>\n<b>UPTIME:</b> <code>{currentTime}</code>\n\n<b>🔺:</b> <code>{ulspeed}/s</code>\<b>🔻:</b> <code>{dlspeed}/s</code>\n"
+        progress += f"\n\n<b>FREE:</b> <code>{free}</code>\n<b>UPTIME:</b> <code>{currentTime}</code>\n\n<b>🔺:</b> <code>{ulspeed}/s</code>\n<b>🔻:</b> <code>{dlspeed}/s</code>\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
